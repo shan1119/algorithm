658b08bb354bc6b668a0c0c3626ee16d48820a47
shan1119 <blood1119@gmail.com>
2020-01-12 14:12:03 +0900

    Update getLongest.java
    another way easy to understand

--- getLongest.java
+++ getLongest.java
@@ -4,6 +4,11 @@ public class getLongest {
     // find out the longest period where work day is not more than 5 during each 7 continuous days.
     // 1 : work day 0 : rest day
     public static void main(String[] args){
+        fun1();
+        fun2();
+
+    }
+    private static void fun1(){
         // test data
         String str = "11111010111101";
         // the continuous work day in each 7 days
@@ -29,5 +34,32 @@ public class getLongest {
 
         System.out.println(max + ", " + current);
         System.out.println(Arrays.toString(day));
+    }
+
+    private static void fun2(){
+        // test data
+        String str = "11111010111101";
+        int[] day = new int[str.length()];
+        int max=0, current=0, check=0;
+
+        for(int i=0;i<str.length()-6;i++){
+            check = 0;
+            for(int j=i;j<i+7 && j<str.length();j++) {
+                check += Integer.parseInt(str.substring(j, j + 1));
+            }
+            System.out.print(check+", ");
+            if(check>5){
+                max = Math.max(max, current);
+                current = 0;
+            }else{
+                if(current == 0){
+                    current = 7;
+                }else{
+                    current++;
+                }
+                max = Math.max(max, current);
+            }
+        }
+        System.out.println(max);
     }
 }
